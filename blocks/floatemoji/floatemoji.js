05d18284d3670b75fb1dc551c2294f40c4d98cdc
Jessica Zacharias <98563366+jzachari@users.noreply.github.com>
2023-05-10 11:37:30 -0700

    Update floatemoji.js

--- blocks/floatemoji/floatemoji.js
+++ blocks/floatemoji/floatemoji.js
@@ -2,7 +2,7 @@ function myMove() {
   let id = null;
   const elem = document.getElementById("animate");
   let pos = 0;
-  let vpos =0;
+  let vpos = 0;
   clearInterval(id);
   id = setInterval(frame, 1);
   var w = window.innerWidth;
