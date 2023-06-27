03855913f9dd7bf1834f14f079df1271825d2250
jzachari <jzachari@adobe.com>
2023-06-26 17:28:30 -0700

    .

--- blocks/shiftbackground/shiftbackground.js
+++ blocks/shiftbackground/shiftbackground.js
@@ -28,7 +28,9 @@ function rgbToHex(r, g, b) {
   }(bin.toString(16).toUpperCase()));
 }
 
-var id = null;
+// Call the shiftColor function every 500 milliseconds and set it to the const id
+var id = setInterval(shiftColor, 500);
+
 // increment the r, g, and b values by 1 until they reach the maxShift value
 function shiftColor() {
   // Set the maxium value that r, b or g can shift by 50
@@ -84,6 +86,3 @@ function shiftColor() {
   // Set the css variable --highlight-background-color to the new color
   document.documentElement.style.setProperty('--highlight-background-color', newhex);
 }
-
-// Call the shiftColor function every 500 milliseconds and set it to the const id
-id = setInterval(shiftColor, 500);
