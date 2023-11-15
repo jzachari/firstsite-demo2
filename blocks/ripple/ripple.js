0c2243e2e586315ef9b985242f9c19f274f212d5
jzachari <jzachari@adobe.com>
2023-11-15 12:10:32 -0800

    .

--- blocks/ripple/ripple.js
+++ blocks/ripple/ripple.js
@@ -1,8 +1,4 @@
 export default function decorate(block) {
-  // Select the picture element
-  const picture = block.querySelector('picture');
-  picture.classList.add('ripple-image');
-
   $('.ripple-image').ripples({
     resolution: 256,
     perturbance: 0.01,
