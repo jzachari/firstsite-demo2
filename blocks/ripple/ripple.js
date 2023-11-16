c40b4cad9e84adc2d2cf9e763dcc8596c6982107
jzachari <jzachari@adobe.com>
2023-11-15 20:02:56 -0800

    .

--- blocks/ripple/ripple.js
+++ blocks/ripple/ripple.js
@@ -1,7 +1,12 @@
-export default function decorate(block) {
-  $('.hero-wrapper').ripples({
-    resolution: 512,
-    radius: 20,
-    perturbance: 0.09,
-  });
-}
+const jqueryScript = document.createElement('script');
+jqueryScript.type = 'text/javascript';
+jqueryScript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+
+document.getElementsByTagName('head')[0]
+  .appendChild(jqueryScript);
+
+$('.hero-wrapper').ripples({
+  resolution: 512,
+  radius: 20,
+  perturbance: 0.09,
+});
