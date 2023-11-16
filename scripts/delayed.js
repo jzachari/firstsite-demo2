fed58c676664b94c41deda899e22f8183ef38f42
jzachari <jzachari@adobe.com>
2023-11-16 15:12:46 -0800

    .

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,29 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import {
+  sampleRUM
+} from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+function jQueryIsReady() {
+  // eslint-disable-next-line no-undef
+  $('.hero-wrapper').ripples({
+    resolution: 512,
+    radius: 20,
+    perturbance: 0.09,
+  });
+}
+
+function checkJquery() {
+  if (typeof jQuery === 'undefined') {
+    return false;
+  }
+  // eslint-disable-next-line no-use-before-define
+  clearTimeout(interval);
+  jQueryIsReady();
+  return true;
+}
+const interval = setInterval(checkJquery, 1000);
